f58c875fc186eae8482f8cc742ce9c8111a9a59e
jamesou9307 <291387044@qq.com>
2017-07-28 08:51:53 +0800

    initial commit

---
+++ src/com/james/jspdemo/entity/Country.java
@@ -0,0 +1,26 @@
+package com.james.jspdemo.entity;
+
+/**
+ * Created by fsuser on 2017-07-28.
+ */
+public class Country {
+
+    private String countryname;
+    private String capital;
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+}
