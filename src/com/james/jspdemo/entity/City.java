b7ae6c5701e1a79ef9d1cd0a866005efad34291c
jamesou9307 <291387044@qq.com>
2017-07-28 08:48:37 +0800

    Rejected commit: Default

---
+++ src/com/james/jspdemo/entity/City.java
@@ -0,0 +1,26 @@
+package com.james.jspdemo.entity;
+
+/**
+ * Created by fsuser on 2017-07-28.
+ */
+public class City {
+
+    private String cityname;
+    private int population;
+
+    public String getCityname() {
+        return cityname;
+    }
+
+    public void setCityname(String cityname) {
+        this.cityname = cityname;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
